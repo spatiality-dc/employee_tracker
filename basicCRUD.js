2ec2cea3e712c2d9c8fdefcfcb0873ced3ce239d
spatiality-dc <dan@dinosaurmedia.org>
2020-05-04 21:14:27 +1000

    CRUD functions for employees written

--- basicCRUD.js
+++ basicCRUD.js
@@ -1,6 +1,7 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -11,7 +12,7 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "B*ffp3t7bX5SkNO>",
-  database: "ice_creamDB",
+  database: "employee_DB",
 });
 
 connection.connect(function (err) {
@@ -20,73 +21,123 @@ connection.connect(function (err) {
   createProduct();
 });
 
-function createProduct() {
-  console.log("Inserting a new product...\n");
-  var query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      flavor: "Rocky Road",
-      price: 3.0,
-      quantity: 50,
-    },
-    function (err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " product inserted!\n");
-      // Call updateProduct AFTER the INSERT completes
-      updateProduct();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
-
-function updateProduct() {
-  console.log("Updating all Rocky Road quantities...\n");
-  var query = connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
+function addEmployee() {
+  inquirer
+    .prompt([
       {
-        quantity: 100,
+        name: "firstName",
+        type: "input",
+        message: "What is the employee's first name?",
       },
       {
-        flavor: "Rocky Road",
+        name: "lastName",
+        type: "input",
+        message: "What is the employee's last name?",
       },
-    ],
-    function (err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " products updated!\n");
-      // Call deleteProduct AFTER the UPDATE completes
-      deleteProduct();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
+      {
+        name: "role",
+        type: "input",
+        message: "What is the employee's role?",
+        choices: [
+          "Salesperson",
+          "Sales Lead",
+          "Software Engineer",
+          "Lead Engineer",
+          "Accountant",
+          "Lawyer",
+          "Legal Team Lead",
+        ],
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,(select id from role where title=?))",
+        [answer.firstName, answer.lastName, answer.role],
+        function (err, res) {
+          if (err) throw err;
+          console.log("----------------------------");
+          console.log(answer.firstName + " has been added to the database.");
+          viewEmployees();
+          console.log("----------------------------");
+          init();
+        }
+      );
+    });
 }
 
-function deleteProduct() {
-  console.log("Deleting all strawberry icecream...\n");
-  connection.query(
-    "DELETE FROM products WHERE ?",
-    {
-      flavor: "strawberry",
-    },
-    function (err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " products deleted!\n");
-      // Call readProducts AFTER the DELETE completes
-      readProducts();
-    }
-  );
+function updateEmployeeRole() {
+  inquirer
+    .prompt([
+      {
+        name: "editEmployee",
+        type: "input",
+        message:
+          "What is the last name of the employee you would like to update?",
+      },
+      {
+        name: "role",
+        type: "input",
+        message: "What is their new role?",
+        choices: [
+          "Salesperson",
+          "Sales Lead",
+          "Software Engineer",
+          "Lead Engineer",
+          "Accountant",
+          "Lawyer",
+          "Legal Team Lead",
+        ],
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "UPDATE employee SET role_id = (select id from role where title=?) WHERE last_name = ?;",
+        [answer.role, answer.editEmployee],
+
+        function (err, res) {
+          if (err) throw err;
+          console.log("----------------------------");
+          console.log(answer.editEmployee + " has had their role updated.");
+          console.log("----------------------------");
+          init();
+        }
+      );
+    });
 }
 
-function readProducts() {
+function deleteEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "deleteEmployee",
+        type: "input",
+        message:
+          "What is the last name of the employees you would like to delete?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "DELETE FROM employee WHERE last_name = ?",
+        [answer.deleteEmployee],
+        function (err, res) {
+          if (err) throw err;
+          console.log("----------------------------");
+          console.log(
+            answer.deleteEmployee + " has been deleted from the database."
+          );
+          console.log("----------------------------");
+          init();
+        }
+      );
+    });
+}
+
+function viewAllEmployees() {
   console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function (err, res) {
+  connection.query("SELECT * FROM employee", function (err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
-    console.log(res);
+    console.table(res);
     connection.end();
   });
 }
